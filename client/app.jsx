ddd6818c214329cd647f2dee5c78c43e0d4bc3d2
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-06-14 23:50:11 +0000

    added addEntry component. added pagecontainer component. added parseroute function to library. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './lib/parse-route';
+import PageContainer from './components/page-container';
+import AddEntry from './pages/add-entry';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '' || route.path === 'home') {
+      return <Home />;
+    }
+    if (route.path === 'add-entry') {
+      return <AddEntry />;
+    }
+  }
+
   render() {
-    return <Home />;
+    return (
+      <PageContainer>
+        {this.renderPage()}
+      </PageContainer>
+    );
   }
 }
