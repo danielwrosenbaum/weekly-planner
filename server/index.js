b2b57341269e95a5f5b7fb918091e9b55235436d
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-06-15 21:57:38 +0000

    setup database. added post request to index.js to successfully post to db. tested with httpie. #1

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 require('dotenv/config');
+
 const express = require('express');
+const db = require('./db');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
 const app = express();
+const jsonMiddleware = express.json();
 
 app.use(staticMiddleware);
+app.use(jsonMiddleware);
 
+app.post('/api/weeklyPlanner', (req, res, next) => {
+  const { day, time, description } = req.body;
+  const sql = `
+  insert into "planner" ("day", "time", "description")
+  values ($1, $2, $3)
+  returning *
+  `;
+  const params = [day, time, description];
+  db.query(sql, params)
+    .then(result => {
+      res.status(201).json(result.rows[0]);
+    })
+    .catch(err => next(err));
+});
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
