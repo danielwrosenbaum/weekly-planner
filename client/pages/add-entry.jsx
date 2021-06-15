68f0495054ba773caed89b2807f3f5a59479d41a
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-06-15 05:09:14 +0000

    added useState to addentry to allow cancel button to remove modal. #1

--- client/pages/add-entry.jsx
+++ client/pages/add-entry.jsx
@@ -1,63 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Home from './home';
 
 export default function AddEntry() {
+  const [isAddClicked, setIsAddClicked] = useState(true);
+
+  function handleCancel() {
+    setIsAddClicked(false);
+  }
+
+  if (!isAddClicked) return <Home />;
   return (
     <div className="modal">
 
-    <div className="modal-container">
-      <form>
-        <div className="row">
-          <div className="col-full">
-            <h1>Add Entry</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-full">
-            <select
-            requiredname="day">
-              <option>--Day of the Week--</option>
-              <option>Monday</option>
-              <option>Tuesday</option>
-              <option>Wednesday</option>
-              <option>Thursday</option>
-              <option>Friday</option>
-              <option>Saturday</option>
-              <option>Sunday</option>
-            </select>
-            <select
-              required
-              name="time">
-              <option>--Time--</option>
-              <option>8:00</option>
-              <option>9:00</option>
-              <option>10:00</option>
-              <option>11:00</option>
-              <option>12:00</option>
-              <option>1:00</option>
-              <option>2:00</option>
-              <option>3:00</option>
-              <option>4:00</option>
-              <option>5:00</option>
-              <option>6:00</option>
-              <option>7:00</option>
-              <option>8:00</option>
-              <option>9:00</option>
-              <option>10:00</option>
-            </select>
+      <div className="modal-container">
+        <div className="modal-box">
+          <form>
             <div className="row">
               <div className="col-full">
-                <textarea
-                  required
-                  name="description"
-                  placeholder="Description"
-                  rows="5"></textarea>
+                <h2>Add Entry</h2>
               </div>
             </div>
-          </div>
+            <div className="row pd-one">
+              <div className="col-full">
+                <select
+                  requiredname="day">
+                  <option>--Day of the Week--</option>
+                  <option>Monday</option>
+                  <option>Tuesday</option>
+                  <option>Wednesday</option>
+                  <option>Thursday</option>
+                  <option>Friday</option>
+                  <option>Saturday</option>
+                  <option>Sunday</option>
+                </select>
+                <select
+                  required
+                  name="time">
+                  <option>--Time--</option>
+                  <option>8:00</option>
+                  <option>9:00</option>
+                  <option>10:00</option>
+                  <option>11:00</option>
+                  <option>12:00</option>
+                  <option>1:00</option>
+                  <option>2:00</option>
+                  <option>3:00</option>
+                  <option>4:00</option>
+                  <option>5:00</option>
+                  <option>6:00</option>
+                  <option>7:00</option>
+                  <option>8:00</option>
+                  <option>9:00</option>
+                  <option>10:00</option>
+                </select>
+                <div className="row pd-one">
+                  <div className="col-full">
+                    <textarea
+                      required
+                      name="description"
+                      placeholder="Description"
+                      rows="5"></textarea>
+                  </div>
+                </div>
+                <div className="row pd-one">
+                  <div className="col-full btn-container">
+                    <button onClick={() => handleCancel()} className="btn-cancel">Cancel</button>
+                    <button type="submit" className="btn-submit">Submit</button>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </form>
+
         </div>
 
-      </form>
-    </div>
+      </div>
     </div>
   );
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -5,13 +5,13 @@ export default function Home(props) {
   const [isAddClicked, setIsAddClicked] = useState(false);
 
   function handleClick() {
-    return setIsAddClicked(true);
-
+    setIsAddClicked(true);
   }
+  if (isAddClicked) return <AddEntry />;
+
   return (
     <>
       <div className="page-container justify-content-ctr col-full align-items-ctr">
-        {(isAddClicked) ? <AddEntry /> : null}
         <div className="button-container justify-content-ctr">
           <button onClick={() => handleClick()} className="add-btn">Add an Entry</button>
       </div>
