05fadd4b98d78db0feac68eac1da0353e80eeb49
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-06-17 01:51:55 +0000

    added buttons for all days to home page. #2

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -21,8 +21,34 @@ export default function Home(props) {
             <h1>Weekly Planner</h1>
           </div>
         </div>
+        <div className="day-btns-container">
+          <div className="row space-evenly wrap">
+            <div className="pd-half align-self-ctr">
+              <button className="day-btns">Sunday</button>
+            </div>
+            <div className="pd-half">
+              <button className="day-btns">Monday</button>
+            </div>
+            <div className="pd-half">
+              <button className="day-btns">Tuesday</button>
+            </div>
+            <div className="pd-half">
+              <button className="day-btns">Wednesday</button>
+            </div>
+            <div className="pd-half">
+              <button className="day-btns">Thursday</button>
+            </div>
+            <div className="pd-half">
+              <button className="day-btns">Friday</button>
+            </div>
+            <div className="pd-half">
+              <button className="day-btns">Saturday</button>
+            </div>
+          </div>
+
+        </div>
         <div className="row justify-content-ctr">
-          <div className="col-full ">
+          <div className="col-full">
             <div className="btn-container justify-content-ctr">
               <button onClick={handleClick} className="add-btn">Add an Entry</button>
             </div>
