2ca224031810755f09573e10aeae5682396dab88
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-07-31 18:26:48 +0000

    added back and forward buttons to week title. #14

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -82,7 +82,8 @@ export default function Home(props) {
 
   // const { start, end, justDayDate } = renderDates();
 
-  const renderTitle = whichDayisClicked[0].toUpperCase() + whichDayisClicked.slice(1);
+  const renderTitle =
+    whichDayisClicked[0].toUpperCase() + whichDayisClicked.slice(1);
   function renderDayOfWeekDate(day) {
     const curr = new Date();
     const start = startOfWeek(curr);
@@ -93,7 +94,10 @@ export default function Home(props) {
     });
     return thisWeek[day];
   }
-  const renderDayTitle = format(renderDayOfWeekDate(whichDayNumberisClicked), 'MM/dd/yyyy');
+  const renderDayTitle = format(
+    renderDayOfWeekDate(whichDayNumberisClicked),
+    'MM/dd/yyyy'
+  );
   const renderWeekTitle = start + ' - ' + end;
 
   const handleDayClick = event => {
@@ -106,89 +110,155 @@ export default function Home(props) {
     if (data) {
       const renderedDays = (
         <tbody>
-          {
-            data.map((entry, index) => {
-              return (
-                <tr key={index}>
-                  <td>{entry.time}</td>
-                  <td>{entry.location}</td>
-                  <td>{entry.description}</td>
-                  <td className="btn-table-row">
-                    <div className="btn-table-container">
-                      <button className="btn-table-edit" value={entry} onClick={() => handleEditClick(entry)}>Edit</button>
-                      <button className="btn-table-delete" value={entry} onClick={() => handleDelete(entry)}>Delete</button>
-                    </div>
-
-                  </td>
-                </tr>
-              );
-            })
-          }
+          {data.map((entry, index) => {
+            return (
+              <tr key={index}>
+                <td>{entry.time}</td>
+                <td>{entry.location}</td>
+                <td>{entry.description}</td>
+                <td className="btn-table-row">
+                  <div className="btn-table-container">
+                    <button
+                      className="btn-table-edit"
+                      value={entry}
+                      onClick={() => handleEditClick(entry)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="btn-table-delete"
+                      value={entry}
+                      onClick={() => handleDelete(entry)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       );
       return renderedDays;
     } else {
       return null;
     }
-
   }
   return (
     <>
       <div className="page-container">
-        {(isAddClicked) && <AddEntry onClick={handleClick} onSubmit={handleNewData} value={whichDayisClicked} date={renderDayTitle} weekStart={startData} />}
-        {(isEditClicked) && <EditEntry value={editEntry} onClick={handleEdit} onSubmit={handleNewData} date={renderDayTitle} weekStart={start} dateNumber={whichDayNumberisClicked} />}
-        {(isDeleteClicked) && <DeleteModal value={editEntry} onClick={handleDelete} />}
+        {isAddClicked && (
+          <AddEntry
+            onClick={handleClick}
+            onSubmit={handleNewData}
+            value={whichDayisClicked}
+            date={renderDayTitle}
+            weekStart={startData}
+          />
+        )}
+        {isEditClicked && (
+          <EditEntry
+            value={editEntry}
+            onClick={handleEdit}
+            onSubmit={handleNewData}
+            date={renderDayTitle}
+            weekStart={start}
+            dateNumber={whichDayNumberisClicked}
+          />
+        )}
+        {isDeleteClicked && (
+          <DeleteModal value={editEntry} onClick={handleDelete} />
+        )}
         <div className="row">
           <div className="col-full centered">
             <h2 className="page-title">Weekly Planner</h2>
-            <h3>{renderWeekTitle}</h3>
+            <div className="row justify-content-ctr">
+              <button>{'<<'}</button>
+              <h3>{renderWeekTitle}</h3>
+              <button>{'>>'}</button>
+            </div>
           </div>
         </div>
         <div className="day-btns-container">
           <div className="row space-evenly wrap">
             <div className="pd-qtr">
-              <button value="sunday" id={0} onClick={handleDayClick} className="day-btns sun-btn">
+              <button
+                value="sunday"
+                id={0}
+                onClick={handleDayClick}
+                className="day-btns sun-btn"
+              >
                 <div>Sunday</div>
                 <div>{parseInt(justDayDate)}</div>
-                </button>
+              </button>
             </div>
             <div className="pd-qtr">
-              <button value="monday" id="1" onClick={handleDayClick} className="day-btns mon-btn">
+              <button
+                value="monday"
+                id="1"
+                onClick={handleDayClick}
+                className="day-btns mon-btn"
+              >
                 <div>Monday</div>
                 <div>{parseInt(justDayDate) + 1}</div>
               </button>
             </div>
             <div className="pd-qtr">
-              <button value="tuesday" id="2" onClick={handleDayClick} className="day-btns tues-btn">
+              <button
+                value="tuesday"
+                id="2"
+                onClick={handleDayClick}
+                className="day-btns tues-btn"
+              >
                 <div>Tuesday</div>
                 <div>{parseInt(justDayDate) + 2}</div>
               </button>
             </div>
             <div className="pd-qtr">
-              <button value="wednesday" id="3" onClick={handleDayClick} className="day-btns wed-btn">
+              <button
+                value="wednesday"
+                id="3"
+                onClick={handleDayClick}
+                className="day-btns wed-btn"
+              >
                 <div>Wednesday</div>
                 <div>{parseInt(justDayDate) + 3}</div>
               </button>
             </div>
             <div className="pd-qtr">
-              <button value="thursday" id="4" onClick={handleDayClick} className="day-btns thurs-btn">
+              <button
+                value="thursday"
+                id="4"
+                onClick={handleDayClick}
+                className="day-btns thurs-btn"
+              >
                 <div>Thursday</div>
                 <div>{parseInt(justDayDate) + 4}</div>
               </button>
             </div>
             <div className="pd-qtr">
-              <button value="friday" id="5" onClick={handleDayClick} className="day-btns fri-btn">
+              <button
+                value="friday"
+                id="5"
+                onClick={handleDayClick}
+                className="day-btns fri-btn"
+              >
                 <div>Friday</div>
-                <div>{parseInt(justDayDate) + 5}</div></button>
+                <div>{parseInt(justDayDate) + 5}</div>
+              </button>
             </div>
             <div className="pd-qtr">
-              <button value="saturday" id="6" onClick={handleDayClick} className="day-btns sat-btn">
+              <button
+                value="saturday"
+                id="6"
+                onClick={handleDayClick}
+                className="day-btns sat-btn"
+              >
                 <div>Saturday</div>
                 <div>{parseInt(justDayDate) + 6}</div>
               </button>
             </div>
           </div>
-
         </div>
 
         <div className="row">
@@ -200,7 +270,10 @@ export default function Home(props) {
         <div className="row">
           <div className="col-full">
             <div className="btn-container">
-              <button onClick={handleClick} className="add-btn"> + Add an Entry</button>
+              <button onClick={handleClick} className="add-btn">
+                {' '}
+                + Add an Entry
+              </button>
             </div>
           </div>
         </div>
